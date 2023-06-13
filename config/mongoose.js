8a42f401db8fe06b5e039bd888f5625702efa434
“Sandip <sandipkalekar808@gmail.com>
2023-06-13 12:31:24 +0530

    CSV UPLOAD Project Complete

---
+++ config/mongoose.js
@@ -0,0 +1,22 @@
+/** ------------------ IMPORTING PACKAGE ------------------ **/
+const mongoose = require("mongoose");
+
+
+/** ------------------ MAKING CONNECTION ------------------ **/
+
+mongoose.connect(process.env.MONGO_URL);
+
+
+//setting it to db
+const db = mongoose.connection;
+
+/** ------------------ CHECKING CONNECTION ------------------ **/
+//if error occurs
+db.on("error", console.error.bind(console, "Error connecting to DB"));
+// when db connects successfully
+db.once("open", function(){
+    console.log("Successfully connected to DB");
+});
+
+/** ------------------ EXPORTING DB ------------------ **/
+module.exports = db;
